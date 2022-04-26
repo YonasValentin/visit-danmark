45436c2a22cdbc38507a9f2d6493bca1451bcce9
YonasValentin <83760534+YonasValentin@users.noreply.github.com>
2022-04-26 12:09:51 +0200

    logo fix

--- src/pages/DiscoverPage.jsx
+++ src/pages/DiscoverPage.jsx
@@ -1,18 +1,18 @@
-import visitdenmark from "../assets/img/VisitDenmark-hvid.png";
+import VisitDenmark from '../assets/img/VisitDenmark-hvid.png';
 
 function DiscoverPage() {
   return (
-    <main className="discover">
+    <main className='discover'>
       <img
-        className="discover__logo"
-        src={visitdenmark}
-        alt="visitdenmark-logo"
+        className='discover__logo'
+        src={VisitDenmark}
+        alt='visitdenmark-logo'
       ></img>
-      <h1 className="discover__heading">Discover</h1>
-      <div className="discover__buttons-container">
-        <button className="discover__button button-reject">Reject</button>
-        <button className="discover__button button-rewind">Rewind</button>
-        <button className="discover__button button-accept">Accept</button>
+      <h1 className='discover__heading'>Discover</h1>
+      <div className='discover__buttons-container'>
+        <button className='discover__button button-reject'>Reject</button>
+        <button className='discover__button button-rewind'>Rewind</button>
+        <button className='discover__button button-accept'>Accept</button>
       </div>
     </main>
   );

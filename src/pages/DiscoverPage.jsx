57c2f5e5c98946cf58540381a4dc99e66b381847
kdk97 <kevinkdk97@gmail.com>
2022-04-26 12:00:36 +0200

    font/billeder

--- src/pages/DiscoverPage.jsx
+++ src/pages/DiscoverPage.jsx
@@ -1,11 +1,18 @@
+import visitdenmark from "../assets/img/VisitDenmark-hvid.png";
+
 function DiscoverPage() {
   return (
-    <main className='discover'>
-      <h1 className='discover__heading'>Discover</h1>
-      <div className='discover__buttons-container'>
-        <button className='discover__button button-reject'>Reject</button>
-        <button className='discover__button button-rewind'>Rewind</button>
-        <button className='discover__button button-accept'>Accept</button>
+    <main className="discover">
+      <img
+        className="discover__logo"
+        src={visitdenmark}
+        alt="visitdenmark-logo"
+      ></img>
+      <h1 className="discover__heading">Discover</h1>
+      <div className="discover__buttons-container">
+        <button className="discover__button button-reject">Reject</button>
+        <button className="discover__button button-rewind">Rewind</button>
+        <button className="discover__button button-accept">Accept</button>
       </div>
     </main>
   );

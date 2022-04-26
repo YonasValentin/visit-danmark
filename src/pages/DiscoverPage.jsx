295b18b682d3dd2fb9bb4373c48cffa455bbd3c8
YonasValentin <83760534+YonasValentin@users.noreply.github.com>
2022-04-26 11:51:27 +0200

    map added

--- src/pages/DiscoverPage.jsx
+++ src/pages/DiscoverPage.jsx
@@ -1,11 +1,13 @@
 function DiscoverPage() {
   return (
     <main className='discover'>
-      <h1 className='discover__heading'>Discover</h1>
-      <div className='discover__buttons-container'>
-        <button className='discover__button button-reject'>Reject</button>
-        <button className='discover__button button-rewind'>Rewind</button>
-        <button className='discover__button button-accept'>Accept</button>
+      <div className='discover__container'>
+        <h1 className='discover__heading'>Discover</h1>
+        <div className='discover__buttons-container'>
+          <button className='discover__button button-reject'>Reject</button>
+          <button className='discover__button button-rewind'>Rewind</button>
+          <button className='discover__button button-accept'>Accept</button>
+        </div>
       </div>
     </main>
   );
